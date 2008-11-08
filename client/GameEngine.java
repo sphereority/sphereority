4b807972889e9f7db6d3d07374ce4b37d3b7cbba
Silumesii Maboshe <silumesii@aluminium.local>
2008-11-08 08:18:15 -0800

    Updated GameEngine getters and setters.

--- client/GameEngine.java
+++ client/GameEngine.java
@@ -1,6 +1,7 @@
 package	client;
 
 import common.Map;
+import javax.swing.JFrame;
 
 /**
  * This class describes the game loop for this game
@@ -10,28 +11,54 @@ import common.Map;
 public class GameEngine {
 	public boolean gameOver;
 	public Map gameMap;
+	public ClientViewArea gameViewArea;
+	public LocalPlayer localPlayer;
+
 
 	// CONSTRUCTORS
 	public GameEngine(Map m) {
 		this.gameOver = false;
 		this.gameMap = m;
+		this.gameViewArea = new ClientViewArea();
 	}
 	
+	
 	// GETTERS
-	public Map getMap() {
+	public Player getLocalPlayer() {
+		return this.localPlayer;
+	}
+	
+	public Map getGameMap() {
 		return this.gameMap;
 	}
 
 	
 	// SETTERS
-	public void setMap(Map m) {
+	public void setLocalPlayer(Player p) {
+		this.localPlayer = p;
+	}
+	
+	public void setGameMap(Map m) {
 		this.gameMap = m;
 	}
 	
 	
 	// OPERATIONS
+	public boolean isGameOver() {
+		return this.gameOver;
+	}
+	
+	public void gameOver() {
+		this.gameOver = true;
+	}
+	
 	public void play() {
 		String title = "Game Engine Test";
 		System.out.println(title);
+		
+		// Set up game window
+		JFrame window = new JFrame(title);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 }
