d276ed9af34c4381cf13844a9799d85376e6fb98
Daryl Van Humbeck <dvanhumb@djvh.(none)>
2008-11-16 13:24:14 -0800

    Fixed misplaced players on the radar display.

--- client/gui/MapRadar.java
+++ client/gui/MapRadar.java
@@ -121,8 +121,8 @@ public class MapRadar extends Widget implements MapChangeListener, Constants
 			
 			px = Math.round(p.getPosition().getX() * scale) + offsetX;
 			py = Math.round(p.getPosition().getY() * scale) + offsetY;
-			px = Math.min(x + width, Math.max(x, px));
-			py = Math.min(y + height, Math.max(y, py));
+			px = Math.min(getFixedX(windowWidth) + width, Math.max(getFixedX(windowWidth), px));
+			py = Math.min(getFixedY(windowHeight) + height, Math.max(getFixedY(windowHeight), py));
 			
 			g.fillRect(px - 1, py - 1, 3, 3);
 		} // end if draw players
