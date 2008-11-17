ffabc775beb8ed2b0f5e9498379c6e0f58160d90
sphereority <smaboshe@sfu.ca>
2008-11-16 17:02:12 -0800

    Decoupled display code from the GameEngine.

--- client/Sphereority.java
+++ client/Sphereority.java
@@ -1,12 +1,14 @@
 package	client;
 
 import common.*;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
 
 /**
  * This is the main client application
  * @author smaboshe
  */
-public class Sphereority extends Thread {
+public class Sphereority extends Thread implements Constants {
 	private GameEngine game;
 	
 	public Sphereority(GameEngine game)
@@ -20,6 +22,28 @@ public class Sphereority extends Thread {
 		//GameEngine game = new GameEngine(new Map("widefield"));
 		//GameEngine game = new GameEngine(new Map("mercury"));
 		GameEngine game = new GameEngine(new Map("random_1"));
+		
+		String title = CLIENT_WINDOW_NAME;
+		
+		// Set up the game window
+		JFrame window = new JFrame(title);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		window.getContentPane().add(game.getGameViewArea(), BorderLayout.CENTER);
+		window.addKeyListener(game.getGameViewArea());
+		
+		game.registerActionListeners(window);
+		
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		
+		
+		
+		
+		
+		
+		
 		Sphereority s = new Sphereority(game);
 		s.start();
 	}
