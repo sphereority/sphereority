06e0848f29070eb31f2665b7740f025d83995809
raceimaztion <dvanhumb@sfu.ca>
2008-11-20 11:14:41 -0800

    Started to fill in the NetConnection.

--- client/net/NetConnection.java
+++ client/net/NetConnection.java
@@ -1,10 +1,20 @@
 package client.net;
 
+import common.*;
 import common.messages.*;
 
-public class NetConnection
+import java.net.*;
+import java.io.*;
+
+import javax.print.attribute.standard.Severity;
+
+public class NetConnection implements Constants
 {
+	protected static String failReason = null;
+	
 	protected int gameId;
+	protected Socket tcpSocket;
+	protected DatagramSocket udpSocket; 
 	
 	private NetConnection()
 	{
@@ -25,6 +35,28 @@ public class NetConnection
 	
 	public static NetConnection connectToServer(String host, String password)
 	{
+		try
+		{
+			Socket tcpSocket = new Socket(host, SERVER_GAME_ENGINE_PORT);
+			if (!tcpSocket.isConnected())
+			{
+				System.err.printf("Failed to connect to server '%s'\n", host);
+				return null;
+			}
+			
+			ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(tcpSocket.getInputStream()));
+			ObjectOutputStream out = new ObjectOutputStream(tcpSocket.getOutputStream());
+			
+		}
+		catch (IOException er)
+		{
+			return null;
+		}
 		return null;
+	}
+	
+	public static String getReasonFailed()
+	{
+		return failReason;
 	}
 }
